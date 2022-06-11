b6ff4f5a2ba40e65ba16a70c7305abc6381458d8
Shawn Toffel <shawn.toffel@gmail.com>
2022-06-11 21:56:59 +0000

    store compressed file on backup dir

--- files/backup.sh
+++ files/backup.sh
@@ -6,22 +6,23 @@ backup_file="$BACKUP_NAME.$timestamp.tar.gz"
 
 echo "starting file backup..."
 
-echo "compressing..."
-tar -zcvf "$backup_file" "$BACKUP_DIRECTORY"
+temp_dir="$BACKUP_DIRECTORY/tmp_backup/$timestamp"
+mkdir -p "$temp_dir"
 
-encrypted_directory="$BACKUP_DIRECTORY/tmp/enc_backup_$timestamp"
+echo "compressing..."
+tar -zcvf "$temp_dir/$backup_file" "$BACKUP_DIRECTORY"
+
+encrypted_directory="$temp_dir/enc"
 mkdir -p "$encrypted_directory"
 
 echo "encrypting..."
-openssl enc -aes-256-cbc -pbkdf2 -pass file:"$ENCRYPTION_KEY_FILE" -in "$backup_file" -out "$encrypted_directory/$backup_file.enc"
+openssl enc -aes-256-cbc -pbkdf2 -pass file:"$ENCRYPTION_KEY_FILE" -in "$temp_dir/$backup_file" -out "$encrypted_directory/$backup_file.enc"
 
 echo "creating upload container..."
 az storage container create --connection-string "$(cat "$AZ_CONNECTION_STRING_FILE")" --name "$AZ_CONTAINER" --only-show-errors
 
 echo "uploading..."
 az storage blob sync --connection-string "$(cat "$AZ_CONNECTION_STRING_FILE")" --container "$AZ_CONTAINER" --source "$encrypted_directory" --delete-destination "$AZ_BLOB_DELETE_DESTINATION"
-
-echo "setting access tiers..."
 
 for path in "$encrypted_directory"/*; do
     name=$(basename "$path")
@@ -30,6 +31,6 @@ for path in "$encrypted_directory"/*; do
 done
 
 echo "cleaning up..."
-rm -r "$encrypted_directory"
+rm -r "$BACKUP_DIRECTORY/tmp_backup"
 
 echo "finished file backup"
